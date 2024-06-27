9fa04105033a9c7956159a9afb6ab30115861384
NITIN KANKANALA <63059530+NITIN0210@users.noreply.github.com>
2024-06-27 15:38:34 -0400

    Add files via upload

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO customer (name) VALUES ('John Doe');
+INSERT INTO customer (name) VALUES ('Nitin K');
+INSERT INTO customer (name) VALUES ('Doe John');
+
+
+INSERT INTO transaction (customerId, amount, date) VALUES (1, 100, '2023-03-10 00:00:00');
+INSERT INTO transaction (customerId, amount, date) VALUES (1, 90, '2023-05-10 00:00:00');
+INSERT INTO transaction (customerId, amount, date) VALUES (2, 120, '2023-05-10 00:00:00');
+INSERT INTO transaction (customerId, amount, date) VALUES (2, 80, '2023-06-10 00:00:00');
+INSERT INTO transaction (customerId, amount, date) VALUES (2, 50, '2023-07-10 00:00:00');
+INSERT INTO transaction (customerId, amount, date) VALUES (3, 90, '2023-03-10 00:00:00');
+INSERT INTO transaction (customerId, amount, date) VALUES (3, 30, '2023-05-10 00:00:00');
+
